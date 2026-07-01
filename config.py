db6c77f91e0b47307cb2f6af5b5cf653afb70b8d
Paolo Andolfo <paolo.andolfo@uber.com>
2026-07-01 11:09:32 +0000

    feat: project scaffolding

---
+++ config.py
@@ -0,0 +1,22 @@
+# fitness-tracker/config.py
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MARATHON_DATE = os.getenv("MARATHON_DATE", "2026-10-18")
+PLAN_START_DATE = os.getenv("PLAN_START_DATE", "2026-07-01")
+SPREADSHEET_ID = os.getenv("SPREADSHEET_ID", "")
+
+STRAVA_CLIENT_ID = os.getenv("STRAVA_CLIENT_ID", "")
+STRAVA_CLIENT_SECRET = os.getenv("STRAVA_CLIENT_SECRET", "")
+STRAVA_REFRESH_TOKEN = os.getenv("STRAVA_REFRESH_TOKEN", "")
+
+WHOOP_CLIENT_ID = os.getenv("WHOOP_CLIENT_ID", "")
+WHOOP_CLIENT_SECRET = os.getenv("WHOOP_CLIENT_SECRET", "")
+WHOOP_REFRESH_TOKEN = os.getenv("WHOOP_REFRESH_TOKEN", "")
+
+GOOGLE_CREDENTIALS_JSON = os.getenv("GOOGLE_CREDENTIALS_JSON", "credentials.json")
+
+RECOVERY_RED_THRESHOLD = 33
+RECOVERY_AMBER_THRESHOLD = 66
